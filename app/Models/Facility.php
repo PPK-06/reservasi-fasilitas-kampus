e516649f8d9662187959a6e3398f3322393ea0d7
Grass-ias <ferdyprasetyaputra08@gmail.com>
2026-09-15 18:02:25 +0700

    feat: tambah model Facility beserta relasi

---
+++ app/Models/Facility.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[Fillable(['name', 'type', 'location', 'capacity', 'description'])]
+class Facility extends Model
+{
+    /**
+     * Get the reservations for the facility.
+     */
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    /**
+     * Get the reports for the facility.
+     */
+    public function reports(): HasMany
+    {
+        return $this->hasMany(Report::class);
+    }
+}
