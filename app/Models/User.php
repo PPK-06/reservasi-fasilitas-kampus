d57f557c1c5d18959186c5b139a105d7acb88713
laangid <elangfadila16@gmail.com>
2026-09-17 21:09:14 +0700

    fix: masukkan role ke Fillable pada model User (F10)

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'identity_number', 'user_type'])]
+#[Fillable(['name', 'email', 'password', 'role', 'identity_number', 'user_type'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
